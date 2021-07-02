7d008c02fe62ebe9bc3f183c088971eb72758d3d
Vitalii <shervit82@gmail.com>
2021-07-02 11:34:31 +0300

    set template

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('app is here...hhhhhhhhhhhhhhh')
+import './styles.css'
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFormHandler)
+
+function submitFormHandler(event) {
+    event.preventDefault()
+
+    console.log(input.value)
+}
